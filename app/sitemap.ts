b76f35a7aa1cc0b9a1cc016b38c225e0837c99a6
Lapocalypsie <jrasolosoa@gmail.com>
2024-08-31 19:11:43 +0200

    feat: Update sitemap URL for about page
    The sitemap.ts file was modified to update the URL for the about page from 'https://zagasy.com/about' to 'https://zagasyko.com/about'. This change ensures that the sitemap includes the correct URL for the about page, improving the accuracy and consistency of the sitemap.

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
             priority: 1,
         },
         {
-            url: 'https://zagasy.com/about',
+            url: 'https://zagasyko.com/about',
             lastModified: new Date(),
             changeFrequency: 'monthly',
             priority: 0.8,
